9ea673a36dae4ec27db5c0c37b9b49c4f70aff61
MrPistol <portenius@gmail.com>
2018-04-18 13:35:18 +0600

    add other Lightcycles

--- src/GameField.java
+++ src/GameField.java
@@ -1,9 +1,8 @@
-import java.awt.Color;
-import java.awt.Graphics2D;
-
 public class GameField {
 
   private int width, height;
+
+
 
   public GameField(int width, int height) {
     this.width = width;
@@ -18,30 +17,18 @@ public class GameField {
     return height;
   }
 
+  public void setWidth(int width) {
+    this.width = width;
+  }
+
+  public void setHeight(int height) {
+    this.height = height;
+  }
+
   public boolean isInside(int x, int y) {
     return x >= 0 && x < width &&
         y >= 0 && y < height;
   }
 
-  public void draw(Graphics2D g2, int screenWidth, int screenHeight) {
-    final int GRID_SIZE = 1;
-    final Color CELL_COLOR = Color.darkGray;
-    int minSideSize = Math.min(screenWidth, screenHeight);
-    int cellWidth = minSideSize / width;
-    int cellHeight = minSideSize / height;
-    int shiftByX = (screenWidth - width * cellWidth) / 2;
-    int shiftByY = (screenHeight - height * cellHeight) / 2;
 
-    g2.setColor(CELL_COLOR);
-    for (int y = 0; y < height; ++y) {
-      for (int x = 0; x < width; ++x) {
-        g2.fillRect(shiftByX + x * cellWidth,
-            shiftByY + y * cellHeight,
-            cellWidth - GRID_SIZE,
-            cellHeight - GRID_SIZE);
-      }
-    }
-
-
-  }
 }
--- src/Tron.java
+++ src/Tron.java
@@ -6,33 +6,113 @@ public class Tron extends JFrame {
 
   String title = "Tron - the arcade game";
   Color background = Color.black;
-  int delay = 10;
+  int delay = 100;
 
-  private final static int GAME_FIELD_WIDTH = 100;
-  private final static int GAME_FIELD_HEIGHT = 100;
+  private final static int GAME_FIELD_WIDTH = 180;
+  private final static int GAME_FIELD_HEIGHT = 180;
+  private final static int GRID_SIZE = 1;
+  private final static Color COLOR_FIELD = Color.darkGray;
+
+
+  private final static int LENGTH_LIGHT_CYCLE_ONE = 1000;
+  private final static int START_X_LIGHT_CYCLE_ONE = 0;
+  private final static int START_Y_LIGHT_CYCLE_ONE = 0;
+  private final static int DX_LIGHT_CYCLE_ONE = 1;
+  private final static int DY_LIGHT_CYCLE_ONE = 0;
+  private final static Color COLOR_LIGHT_CYCLE_ONE = Color.yellow;
+
+  private final static int LENGTH_LIGHT_CYCLE_TWO = 1000;
+  private final static int START_X_LIGHT_CYCLE_TWO = 0;
+  private final static int START_Y_LIGHT_CYCLE_TWO = 89;
+  private final static int DX_LIGHT_CYCLE_TWO = 1;
+  private final static int DY_LIGHT_CYCLE_TWO = 0;
+  private final static Color COLOR_LIGHT_CYCLE_TWO = Color.cyan;
+
+
+  private MathField mathField;
+
   private GameField gameField;
+  private DrawGameField drawGameField;
+
+  private LightCycle lightCycle;
+  private DrawLightCycle drawLightCycle;
+
+  private LightCycle lightCycleSecond;
+  private DrawLightCycle drawLightCycleSecond;
 
   void start() {
+
+    mathField = new MathField(getWidth(), getHeight(), GAME_FIELD_WIDTH, GAME_FIELD_HEIGHT,
+        GRID_SIZE);
+
     gameField = new GameField(GAME_FIELD_WIDTH, GAME_FIELD_HEIGHT);
+    drawGameField = new DrawGameField(COLOR_FIELD);
+
+    lightCycle = new LightCycle(LENGTH_LIGHT_CYCLE_ONE, START_X_LIGHT_CYCLE_ONE,
+        START_Y_LIGHT_CYCLE_ONE, DX_LIGHT_CYCLE_ONE, DY_LIGHT_CYCLE_ONE);
+    drawLightCycle = new DrawLightCycle(COLOR_LIGHT_CYCLE_ONE);
+
+    lightCycleSecond = new LightCycle(LENGTH_LIGHT_CYCLE_TWO, START_X_LIGHT_CYCLE_TWO,
+        START_Y_LIGHT_CYCLE_TWO, DX_LIGHT_CYCLE_TWO, DY_LIGHT_CYCLE_TWO);
+    drawLightCycleSecond = new DrawLightCycle(COLOR_LIGHT_CYCLE_TWO);
+
 
   }
 
   void update() {
     // код для обновления свойств объектов
+    lightCycle.move(gameField, lightCycle, lightCycleSecond);
+    lightCycleSecond.move(gameField, lightCycle, lightCycleSecond);
 
   }
 
   void draw(Graphics2D g2) {
+    if (drawGameField != null) {
+      drawGameField.grid(g2);
+    }
 
-    gameField.draw(g2, getWidth(),getHeight());
+    if (drawLightCycle != null) {
+      drawLightCycle.draw(g2, lightCycle);
+    }
+    if (drawLightCycleSecond != null) {
+      drawLightCycleSecond.draw(g2, lightCycleSecond);
+    }
 
   }
 
   void input(int keyCode) {
-    // код для обработки ввода
-    if (keyCode == KeyEvent.VK_ESCAPE) {
-      System.exit(0);
-    } // else if / switch...
+    switch (keyCode) {
+      case (KeyEvent.VK_UP):
+        lightCycle.turnUp();
+        break;
+      case (KeyEvent.VK_RIGHT):
+        lightCycle.turnRight();
+        break;
+      case (KeyEvent.VK_DOWN):
+        lightCycle.turnDown();
+        break;
+      case (KeyEvent.VK_LEFT):
+        lightCycle.turnLeft();
+        break;
+
+      case (KeyEvent.VK_W):
+        lightCycleSecond.turnUp();
+        break;
+      case (KeyEvent.VK_D):
+        lightCycleSecond.turnRight();
+        break;
+      case (KeyEvent.VK_S):
+        lightCycleSecond.turnDown();
+        break;
+      case (KeyEvent.VK_A):
+        lightCycleSecond.turnLeft();
+        break;
+
+      case (KeyEvent.VK_ESCAPE):
+        System.exit(0);
+        break;
+    }
+
 
   }
 
@@ -51,16 +131,21 @@ public class Tron extends JFrame {
       }
     });
     add(panel);
+    addComponentListener(new ComponentAdapter() {
+      @Override
+      public void componentShown(ComponentEvent e) {
+        super.componentShown(e);
+        start();
 
-    start();
-
-    javax.swing.Timer timer = new javax.swing.Timer(delay, new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        update();
-        repaint();
+        javax.swing.Timer timer = new javax.swing.Timer(delay, new ActionListener() {
+          public void actionPerformed(ActionEvent e) {
+            update();
+            repaint();
+          }
+        });
+        timer.start();
       }
     });
-    timer.start();
 
     setVisible(true);
   }
